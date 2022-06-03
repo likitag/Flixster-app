1b39d1e9f91bf963d305d1d7a9a11d7c747686ec
Likita Gangireddy <likitag@fb.com>
2022-06-03 13:33:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Movie.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+
+
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
+
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
+
+    }
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0 ; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+
+        }
+        return movies;
+
+
+
+    }
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+}
